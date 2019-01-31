e2ef6d524cf12916a9dd685d4694a1b23d170861
srk <er.pathanshahrukh@gmail.com>
2019-01-31 23:27:26 +0530

    direcorty

--- app/src/main/java/currency/recognize/currencyrecog/MainActivity.java
+++ app/src/main/java/currency/recognize/currencyrecog/MainActivity.java
@@ -7,23 +7,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//sstem out print
-        //askdfs
-        //hell0 aay
-        //changes from shshrukh
-        // Example of a call to a native method
-
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
     public void mainemthod()
     {
